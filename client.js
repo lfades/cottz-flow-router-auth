ed398018ee9f74ddae7725c2246a3cb8b78e3af4
Luis <luisito453@gmail.com>
2015-08-11 10:14:11 -0500

    there is no need of an autorun on the client

--- client.js
+++ client.js
@@ -1,17 +1,3 @@
-var oldpath = '';
-
-Tracker.autorun(function () {
-	FlowRouter.watchPathChange();
-	var route = FlowRouter.current().route;
-	if (!route) return;
-
-	var path = route.path;
-	if (path === oldpath) return;
-	oldpath = path; 
-
-	FlowRouter.Auth.check(route.name || path, route.group);
-});
-
 FlowRouter.triggers.enter(function (context, redirect) {
 	var route = context.route;
 	FlowRouter.Auth.check(route.name || route.path, route.group, redirect);
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cottz:flow-router-auth',
-  version: '0.0.7',
+  version: '0.0.8',
   // Brief, one-line summary of the package.
   summary: 'simple way to validate authentications with Flow-Router',
   // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.1.0.3');
 
   api.use('tracker');
   api.use('mongo');
